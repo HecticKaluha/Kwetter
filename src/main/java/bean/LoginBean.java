d745e46e70a9edbb45da3ae7f050a6a4e50ac3db
HecticKaluha <stefanoverhoeve44@gmail.com>
2018-03-25 18:35:08 +0200

    added some code to login
    it's not working yet

--- src/main/java/bean/LoginBean.java
+++ src/main/java/bean/LoginBean.java
@@ -6,6 +6,8 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
 
 @RequestScoped
@@ -49,26 +51,30 @@ public class LoginBean implements Serializable{
         this.usernameComponent = usernameComponent;
     }
 
-    public void login()
+    public String login()
     {
-        try{
-            ExternalContext externalContext = FacesContext.getCurrentInstance()
-                    .getExternalContext();
-            if(this.username.equals("admin") && this.password.equals("admin")) {
-                externalContext.redirect(externalContext.getRequestContextPath()
-                        + "/index.xhtml");
-            }
-            else
-            {
-                FacesContext context = FacesContext.getCurrentInstance();
-                context.addMessage(usernameComponent.getClientId(), new FacesMessage("Username may not be correct"));
-                context.addMessage(passwordComponent.getClientId(), new FacesMessage("Password may not be correct"));
-            }
-        }
-        catch(Exception e)
-        {
-            System.out.print("Something went wrong because: " + e.getMessage());
-        }
-    }
+        System.out.print("Handling login");
 
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        System.out.print("Got Context");
+
+        HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();
+
+        System.out.print("got request");
+
+        try{
+            System.out.print("Login in...");
+            request.login(this.username, this.password);
+            System.out.print("Logged in");
+        }
+        catch(Exception e){
+            System.out.print("Something went wrong because: " + e.getMessage());
+            context.addMessage(usernameComponent.getClientId(), new FacesMessage("Username may not be correct"));
+            context.addMessage(passwordComponent.getClientId(), new FacesMessage("Password may not be correct"));
+            return "/error/forbidden.xhtml";
+        }
+        System.out.print("Login Succesvol");
+        return "/pages/admin.xhtml";
+    }
 }
